24115eae4366941093734ef0780131797b552ae4
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2022-09-28 18:25:34 +0800

    Update groups.js

--- Spookhost/Upptime/groups.js
+++ Spookhost/Upptime/groups.js
@@ -1,5 +1,8 @@
-let coreservices = document.createElement("h1");
-let coreservicesp = document.createElement("p");
-coreservices.appendChild(coreservicesp);
-document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)").parentNode.insertBefore(coreservices, document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)"));
-coreservicesp.after("Core Services");
+if (document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)") != null) {
+  let coreservices = document.createElement("h1");
+  let coreservicesp = document.createElement("p");
+  coreservices.appendChild(coreservicesp);
+  document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)").parentNode.insertBefore(coreservices, document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)"));
+  coreservicesp.after("Core Services");
+
+}
