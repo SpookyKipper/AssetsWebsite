87b3d69e735daf6dca27f4e0dc1d6b7eac573c4f
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2022-09-28 21:39:13 +0800

    Update groups.js

--- Spookhost/Upptime/groups.js
+++ Spookhost/Upptime/groups.js
@@ -4,7 +4,7 @@ function grouping1() {
     let coreservicesp = document.createElement("p");
     coreservices.appendChild(coreservicesp);
     document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)").parentNode.insertBefore(coreservices, document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)"));
-    coreservicesp.after("Core Services");
+    coreservicesp.after("Essential Services");
   } else {
     setTimeout(grouping1, 100)
   }
