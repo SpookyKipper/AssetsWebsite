51dc6cf44fcdc109f853e4794a6da52353cd01dd
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2022-09-28 21:18:33 +0800

    Update groups.js

--- Spookhost/Upptime/groups.js
+++ Spookhost/Upptime/groups.js
@@ -11,4 +11,33 @@ function grouping1() {
 
 }
 
-setTimeout(grouping1, 100)
+function grouping2() {
+  if (document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(7)") != null) {
+    let coreservices = document.createElement("h1");
+    let coreservicesp = document.createElement("p");
+    coreservices.appendChild(coreservicesp);
+    document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(7)").parentNode.insertBefore(coreservices, document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)"));
+    coreservicesp.after("Core Services");
+  } else {
+    setTimeout(grouping2, 100)
+  }
+
+}
+
+
+function grouping3() {
+  if (document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(9)") != null) {
+    let coreservices = document.createElement("h1");
+    let coreservicesp = document.createElement("p");
+    coreservices.appendChild(coreservicesp);
+    document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(9)").parentNode.insertBefore(coreservices, document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)"));
+    coreservicesp.after("Core Services");
+  } else {
+    setTimeout(grouping3, 100)
+  }
+
+}
+
+setTimeout(grouping3, 100);
+setTimeout(grouping2, 100);
+setTimeout(grouping1, 100);
