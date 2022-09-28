f0b13ba31b65eb2de8935532ce3150fe2be01506
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2022-09-28 18:26:22 +0800

    Update groups.js

--- Spookhost/Upptime/groups.js
+++ Spookhost/Upptime/groups.js
@@ -1,8 +1,12 @@
-if (document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)") != null) {
-  let coreservices = document.createElement("h1");
-  let coreservicesp = document.createElement("p");
-  coreservices.appendChild(coreservicesp);
-  document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)").parentNode.insertBefore(coreservices, document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)"));
-  coreservicesp.after("Core Services");
+function grouping1() {
+  if (document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)") != null) {
+    let coreservices = document.createElement("h1");
+    let coreservicesp = document.createElement("p");
+    coreservices.appendChild(coreservicesp);
+    document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)").parentNode.insertBefore(coreservices, document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)"));
+    coreservicesp.after("Core Services");
+  } else {
+    setTimeout(grouping1, 100)
+  }
 
 }
