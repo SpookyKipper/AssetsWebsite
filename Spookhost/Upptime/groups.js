1ac44749c3dad663d5139629bd27c5818b84acb8
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2022-09-28 17:57:13 +0800

    Create groups.js

---
+++ Spookhost/Upptime/groups.js
@@ -0,0 +1,5 @@
+let coreservices = document.createElement("h1");
+let coreservicesp = document.createElement("p");
+coreservices.appendChild(coreservicesp);
+document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)").parentNode.insertBefore(coreservices, document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)"));
+coreservicesp.after("Core Services");
