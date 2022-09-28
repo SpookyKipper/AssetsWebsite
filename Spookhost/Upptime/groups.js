e54f7a8b238388f6925060cc00fe44d4584399e9
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2022-09-28 21:23:28 +0800

    Update groups.js

--- Spookhost/Upptime/groups.js
+++ Spookhost/Upptime/groups.js
@@ -13,25 +13,24 @@ function grouping1() {
 
 function grouping2() {
   if (document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(7)") != null) {
-    let coreservices = document.createElement("h1");
-    let coreservicesp = document.createElement("p");
-    coreservices.appendChild(coreservicesp);
-    document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(7)").parentNode.insertBefore(coreservices, document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)"));
-    coreservicesp.after("Core Services");
+    let triageservices = document.createElement("h1");
+    let triageservicesp = document.createElement("p");
+    triageservices.appendChild(triageservicesp);
+    document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(7)").parentNode.insertBefore(triageservices, document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)"));
+    triageservicesp.after("Core Services");
   } else {
     setTimeout(grouping2, 100)
   }
 
 }
 
-
 function grouping3() {
   if (document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(9)") != null) {
-    let coreservices = document.createElement("h1");
-    let coreservicesp = document.createElement("p");
-    coreservices.appendChild(coreservicesp);
-    document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(9)").parentNode.insertBefore(coreservices, document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)"));
-    coreservicesp.after("Core Services");
+    let otherservices = document.createElement("h1");
+    let otherservicesp = document.createElement("p");
+    otherservices.appendChild(otherservicesp);
+    document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(9)").parentNode.insertBefore(otherservices, document.querySelector("#sapper > main > section.live-status.svelte-fqsq6s > article:nth-child(1)"));
+    otherservicesp.after("Core Services");
   } else {
     setTimeout(grouping3, 100)
   }
