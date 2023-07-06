9bd63941e6a6758afd36e0ab38266fc6a13bf511
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2023-07-06 16:10:23 +0800

    Update tsMode.js

--- Spookhost/FileManager/tsMode.js
+++ Spookhost/FileManager/tsMode.js
@@ -1,3 +1,5 @@
+/*Spooky Kipper Injection for Spookhost */
+document.title = "Spookhost File Manager";
 /*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * monaco-typescript version: 3.2.0(f5602dc25acf6ce6f56dd4cc6211dc6aff131db0)
