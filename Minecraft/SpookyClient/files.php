6210f47c97fa96e214a2dee478fd38401f17cc37
Spooky Kipper <me@spooky.hk>
2023-10-13 21:26:01 +0800

    e
    e

---
+++ Minecraft/SpookyClient/files.php
@@ -0,0 +1,158 @@
+[
+    {
+        "path": "natives\/",
+        "checksumSHA1": false,
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/natives\/"
+    },
+    {
+        "path": "natives\/avutil-ttv-51.dll",
+        "checksumSHA1": "97ae5816326546ef5672ec795d4e79e75d9298d7",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/natives\/avutil-ttv-51.dll"
+    },
+    {
+        "path": "natives\/jinput-dx8.dll",
+        "checksumSHA1": "50348b2958b017df3bf30d7915ab61a4cb9a2b33",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/natives\/jinput-dx8.dll"
+    },
+    {
+        "path": "natives\/jinput-dx8_64.dll",
+        "checksumSHA1": "5d9a7d3fb6224dab97aaff7bd9430232732d9be8",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/natives\/jinput-dx8_64.dll"
+    },
+    {
+        "path": "natives\/jinput-raw.dll",
+        "checksumSHA1": "4d982f408e815519c2289cd720c78338392a9887",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/natives\/jinput-raw.dll"
+    },
+    {
+        "path": "natives\/jinput-raw_64.dll",
+        "checksumSHA1": "067c11135f9ebeb554d5f80b7a8a5244c0f3b7d7",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/natives\/jinput-raw_64.dll"
+    },
+    {
+        "path": "natives\/jinput-wintab.dll",
+        "checksumSHA1": "e7b9beead279298611d0c4753089d3af07c4c9e9",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/natives\/jinput-wintab.dll"
+    },
+    {
+        "path": "natives\/libmp3lame-ttv.dll",
+        "checksumSHA1": "cc0c9bf8e76734d23313ec83efba7440dce31b91",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/natives\/libmp3lame-ttv.dll"
+    },
+    {
+        "path": "natives\/lwjgl.dll",
+        "checksumSHA1": "6f63e2a98d9bd272e99eec0f4d453a25795298e5",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/natives\/lwjgl.dll"
+    },
+    {
+        "path": "natives\/lwjgl64.dll",
+        "checksumSHA1": "d8bc4e20accb989fe9d774ede6c198781c2067c7",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/natives\/lwjgl64.dll"
+    },
+    {
+        "path": "natives\/OpenAL32.dll",
+        "checksumSHA1": "ede381bf55e7d0cd3a7e058237bbc66a8ff63837",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/natives\/OpenAL32.dll"
+    },
+    {
+        "path": "natives\/OpenAL64.dll",
+        "checksumSHA1": "97362fba53dfb6d9581b8c64829f4b1d98a97855",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/natives\/OpenAL64.dll"
+    },
+    {
+        "path": "natives\/swresample-ttv-0.dll",
+        "checksumSHA1": "25d1923a17511a9fc1ef56c510dfb63621826274",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/natives\/swresample-ttv-0.dll"
+    },
+    {
+        "path": "natives\/twitchsdk.dll",
+        "checksumSHA1": "ff8f9c1a828241b1643fe5b0ecdaf2d4d86b8daa",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/natives\/twitchsdk.dll"
+    },
+    {
+        "path": "SpookyClient-natives\/",
+        "checksumSHA1": false,
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/SpookyClient-natives\/"
+    },
+    {
+        "path": "SpookyClient-natives\/.json",
+        "checksumSHA1": "b025122d691193e10a691508156da411cdfe55ff",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/SpookyClient-natives\/.json"
+    },
+    {
+        "path": "SpookyClient-natives\/avutil-ttv-51.dll",
+        "checksumSHA1": "97ae5816326546ef5672ec795d4e79e75d9298d7",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/SpookyClient-natives\/avutil-ttv-51.dll"
+    },
+    {
+        "path": "SpookyClient-natives\/jinput-dx8.dll",
+        "checksumSHA1": "50348b2958b017df3bf30d7915ab61a4cb9a2b33",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/SpookyClient-natives\/jinput-dx8.dll"
+    },
+    {
+        "path": "SpookyClient-natives\/jinput-dx8_64.dll",
+        "checksumSHA1": "5d9a7d3fb6224dab97aaff7bd9430232732d9be8",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/SpookyClient-natives\/jinput-dx8_64.dll"
+    },
+    {
+        "path": "SpookyClient-natives\/jinput-raw.dll",
+        "checksumSHA1": "4d982f408e815519c2289cd720c78338392a9887",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/SpookyClient-natives\/jinput-raw.dll"
+    },
+    {
+        "path": "SpookyClient-natives\/jinput-raw_64.dll",
+        "checksumSHA1": "067c11135f9ebeb554d5f80b7a8a5244c0f3b7d7",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/SpookyClient-natives\/jinput-raw_64.dll"
+    },
+    {
+        "path": "SpookyClient-natives\/jinput-wintab.dll",
+        "checksumSHA1": "e7b9beead279298611d0c4753089d3af07c4c9e9",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/SpookyClient-natives\/jinput-wintab.dll"
+    },
+    {
+        "path": "SpookyClient-natives\/libmp3lame-ttv.dll",
+        "checksumSHA1": "cc0c9bf8e76734d23313ec83efba7440dce31b91",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/SpookyClient-natives\/libmp3lame-ttv.dll"
+    },
+    {
+        "path": "SpookyClient-natives\/lwjgl.dll",
+        "checksumSHA1": "6f63e2a98d9bd272e99eec0f4d453a25795298e5",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/SpookyClient-natives\/lwjgl.dll"
+    },
+    {
+        "path": "SpookyClient-natives\/lwjgl64.dll",
+        "checksumSHA1": "d8bc4e20accb989fe9d774ede6c198781c2067c7",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/SpookyClient-natives\/lwjgl64.dll"
+    },
+    {
+        "path": "SpookyClient-natives\/OpenAL32.dll",
+        "checksumSHA1": "ede381bf55e7d0cd3a7e058237bbc66a8ff63837",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/SpookyClient-natives\/OpenAL32.dll"
+    },
+    {
+        "path": "SpookyClient-natives\/OpenAL64.dll",
+        "checksumSHA1": "97362fba53dfb6d9581b8c64829f4b1d98a97855",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/SpookyClient-natives\/OpenAL64.dll"
+    },
+    {
+        "path": "SpookyClient-natives\/swresample-ttv-0.dll",
+        "checksumSHA1": "25d1923a17511a9fc1ef56c510dfb63621826274",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/SpookyClient-natives\/swresample-ttv-0.dll"
+    },
+    {
+        "path": "SpookyClient-natives\/twitchsdk.dll",
+        "checksumSHA1": "ff8f9c1a828241b1643fe5b0ecdaf2d4d86b8daa",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/SpookyClient-natives\/twitchsdk.dll"
+    },
+    {
+        "path": "SpookyClient.json",
+        "checksumSHA1": "8389873d269fe203913539e28a834fa4648af16b",
+        "url": "https:\/\/cdn.spookykip.xyz\/Minecraft\/SpookyClient\/SpookyClient.json"
+    },
+    {
+        "dirCheckUselessFiles": "mods"
+    },
+    {
+        "dirCheckUselessFiles": "config"
+    }
+]
