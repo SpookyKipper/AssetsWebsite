c9bfb2878b5bf56c9e77322ac8bebde241620da0
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2024-09-27 22:54:24 +0800

    Update darkmode.js

--- mainProfileScripts/darkmode.js
+++ mainProfileScripts/darkmode.js
@@ -1,4 +1,9 @@
- function addDarkmodeWidget() {
-    new Darkmode().showWidget();
+const options = {
+  time: '0.5s', // default: '0.3s'
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+function addDarkmodeWidget() {
+    new Darkmode(options).showWidget();
   }
   window.addEventListener('load', addDarkmodeWidget);
