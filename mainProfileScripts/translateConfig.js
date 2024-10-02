6073f66fe6b5a02ad442006aaec889a6f859272e
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2024-10-02 17:11:24 +0800

    Update and rename translator.js to translateConfig.js

---
+++ mainProfileScripts/translateConfig.js
@@ -0,0 +1,1 @@
+window.gtranslateSettings = {"default_language":"en","native_language_names":true,"detect_browser_language":true,"languages":["en","zh-TW","zh-CN"],"wrapper_selector":".gtranslate_wrapper"}
