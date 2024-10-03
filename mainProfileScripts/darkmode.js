44c68e17b942a0782f49507196801b2efae6ebb3
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2024-10-03 20:32:39 +0800

    Update darkmode.js

--- mainProfileScripts/darkmode.js
+++ mainProfileScripts/darkmode.js
@@ -1,9 +1,23 @@
-const options = {
-  time: '0.5s', // default: '0.3s'
-  label: '🌓', // default: ''
-  autoMatchOsTheme: true // default: true
+let dark = false
+function darkModeToggle() {
+    console.log("Dark Mode: " + dark);
+    if (dark == false) {
+        const darkModeCSS = document.createElement("style");
+        darkModeCSS.id = "darkmodecss";
+        darkModeCSS.innerHTML = `
+        html, img, .cert-card::before, .conveythis-widget-language > div:nth-child(1), .darkmode-toggle {
+            filter: invert(1);
+        }
+        ::selection {
+            background-color: #8aff00 !important;
+            color: black;
+        }
+        `;
+        document.head.appendChild(darkModeCSS);
+        dark = true;
+    } else {
+        document.getElementById("darkmodecss")?.remove();
+        dark = false;
+    }
 }
-function addDarkmodeWidget() {
-    new Darkmode(options).showWidget();
-  }
-  window.addEventListener('load', addDarkmodeWidget);
+darkModeToggle();
